33ca14a042e6f6c3d7b16aab949baa71364060c0
syed raiyan ali <syedraiyanali23544@gmail.com>
2026-04-11 13:25:29 +0530

    Optimization: Reduced asset size and updated Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // This will create .gz versions of your 16MB assets so 
+    // modern browsers can download them even faster.
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  build: {
+    // We turn sourcemaps off for the final build to save space
+    sourcemap: false, 
+    // This helps manage large assets like your GIFs
+    chunkSizeWarningLimit: 2000, 
+    rollupOptions: {
+      output: {
+        // This organizes your dist folder better
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
